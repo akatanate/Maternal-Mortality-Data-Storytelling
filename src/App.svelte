1bfab0011d0ee1caf0546fc71003a989d0bd8fab
akatanate <rinmunakata@gmail.com>
2025-07-21 19:02:46 -0700

    finished ordering of sections, body size

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,40 @@
 <script>
-  import LeftAlignedExample from "./sections/LeftAlignedExample.svelte";
-  import RightAlignedExample from "./sections/RightAlignedExample.svelte";
-  import ImageExample from "./sections/ImageExample.svelte";
+  // import LeftAlignedExample from "./sections/LeftAlignedExample.svelte";
+  // import RightAlignedExample from "./sections/RightAlignedExample.svelte";
+  // import ImageExample from "./sections/ImageExample.svelte";
+
+  // import Scroll3_Graph2 from "./sections/Scroll3_Graph2.svelte";
+  // import Scroll4_Graph3 from "./sections/Scroll4_Graph3.svelte";
+  // import Scroll5_Graph4 from "./sections/Scroll5_Graph4.svelte";
+  // import Data1 from "./sections/data1.svelte";
+  // import Data2 from "./sections/data2.svelte";
+
   import TitleSection from "./sections/TitleSection.svelte";
-  import TestSection from "./sections/TestSection.svelte";
+  import Scroll_1TV from "./sections/Scroll1_TV.svelte";
+  import Scroll2_Graph1 from "./sections/Scroll2_Graph1.svelte";
   import Section1 from "./sections/Section1.svelte";
   import Section2 from "./sections/Section2.svelte";
+  import DataTest from "./sections/data_test.svelte";
+  import Closing from "./sections/Closing.svelte";
 </script>
 
 <main>
+  <!--Hook-->
   <TitleSection />
   <Section1  />
   <Section2  />
   
-  <TestSection />
+  <!--Bowie-->
+  <Scroll_1TV />
 
-  <RightAlignedExample />
-  <LeftAlignedExample />
-  <ImageExample />
+  <!--Mortality Rate-->
+  <Scroll2_Graph1 />
+  
+  <!--BWDC 1: available care-->
+  <!--BWDC 2: location & insurances barriers-->
+  <!--BWDC 3: disproportionate harm-->
+  <DataTest/>
+
+  <Closing /> 
+
 </main>
